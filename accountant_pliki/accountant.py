fea2c8d7fec2df7a213926657dc8ebdfa3a21818
githubMMentel <radioslava.pl@gmail.com>
2020-11-13 22:54:24 +0100

    zmiana nazwy oryginalnego pliku out,

---
+++ accountant_pliki/accountant.py
@@ -0,0 +1,28 @@
+#import sys
+#tryb = sys.argv[1]
+
+    #zmienne początkowe
+saldo = 0
+id_prod = 0
+ile_prod = 0
+cena_prod = 0
+
+inp_dict = dict()
+inp_dict = input(in.txt)
+print(inp_dict)
+
+
+#if saldo:
+
+#if zakup:
+
+#if sprzedaz:
+#    id_prod = input(str())
+#    cena_prod = input(int())
+#    ile_prod = input(int())
+
+#    saldo = saldo + (cena_prod * ile_prod)
+else:
+    print("Nieprawidłowe dane. Koniec.")
+
+

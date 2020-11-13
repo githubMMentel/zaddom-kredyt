ba5334b7dc2d4b2cedbd0ab830afe43da7cfeef8
githubMMentel <radioslava.pl@gmail.com>
2020-11-13 22:37:22 +0100

    usuniecie starych plikow in/out z kat zat3,

---
+++ accountant.py
@@ -0,0 +1,30 @@
+
+import sys
+tryb = sys.argv[1]
+
+
+#zmienne początkowe
+
+saldo = 0
+id_prod = 0
+ile_prod = 0
+cena_prod = 0
+
+
+
+
+if saldo:
+
+
+if zakup:
+
+if sprzedaz:
+    id_prod = input(str())
+    cena_prod = input(int())
+    ile_prod = input(int())
+
+    saldo = saldo + (cena_prod * ile_prod)
+else:
+    print("Nieprawidłowe dane. Koniec.")
+
+
